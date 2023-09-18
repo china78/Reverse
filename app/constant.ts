e17fcb4f31e69dd0bd521b05c0eb610ef7e2e309
tgg <444202104@qq.com>
2023-09-18 16:11:59 +0800

    pro: 8000

--- app/constant.ts
+++ app/constant.ts
@@ -160,8 +160,8 @@ export const PRINCES: PRINCES[] = [
 ];
 
 export const CASDOOR = {
-  endpoint: "http://localhost:7001", // 开发
-  // endpoint: "http://localhost:8000", // 生产
+  // endpoint: "http://localhost:7001", // 开发
+  endpoint: "http://localhost:8000", // 生产
   clientId: "c6b3a0adcc44d5d10856",
   appName: "gpt_test",
   organizationName: "built-in",

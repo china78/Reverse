eaec6163732418e5b2a8172f8a6f146c33489694
tgg <444202104@qq.com>
2023-09-28 16:27:29 +0800

    feat: 添加 sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a04f1746f305accb1c1a1ae7b1547518@o4505956948049920.ingest.sentry.io/4505956952375296",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
